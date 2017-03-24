24d3e860d89156d91fe77fab81810293416c1f9b
Clifton Li <Clifton.Li@ucsf.edu>
2017-03-24 16:22:01 -0700

    updated new OID values

--- src/main/java/org/rsna/isn/util/Constants.java
+++ src/main/java/org/rsna/isn/util/Constants.java
@@ -1,4 +1,4 @@
-/* Copyright (c) <2010>, <Radiological Society of North America>
+/* Copyright (c) <2017>, <Radiological Society of North America>
  * All rights reserved.
  * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
  * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
@@ -27,30 +27,27 @@ package org.rsna.isn.util;
  * This class contains constants required by multiple apps.
  *
  * @author Wyatt Tellis
- * @version 2.1.0
+ * @author Clifton Li
+ * @version 5.0.0
+ * @since 2.1.0
  */
 public class Constants
 {
 	
 	/**
-	 * The assigning authority universal id assigned to the RSNA image sharing
-	 * network.
-	 */
-	public static final String RSNA_UNIVERSAL_ID = "1.3.6.1.4.1.19376.2.840.1.1.1.1";
-	/**
-	 * The assigning authority universal id assigned to the RSNA image sharing
-	 * network.
-	 */
-	public static final String RSNA_ISN_UNIVERSAL_ID = "1.3.6.1.4.1.19376.3.840.1.1";    
-	/**
 	 * Identifies the Assigning Authority used for patient identifiers 
          * sent to the Clearinghouse
 	 */
-	public static final String RSNA_ISN_ASSIGNING_AUTHORITY = "1.3.6.1.4.1.19376.3.840.1.1.1";                    
+	public static final String RSNA_ISN_ASSIGNING_AUTHORITY = "1.3.6.1.4.1.19376.3.840.1.1.1";  
+	/**
+	 * Identifies the root for the sourceId in the submission set. 
+         * Individual document sources will be assigned a unique value based on this root.
+	 */        
+        public static final String RSNA_ISN_ROOT_SOURCE_ID = "1.3.6.1.4.1.19376.3.840.1.1.4";  
         /**
 	 * The assigning authority universal id type assigned to the RSNA image sharing
 	 * network.
-	 */
+	 */                  
 	public static final String RSNA_UNIVERSAL_ID_TYPE = "ISO";
 
 	private Constants()
